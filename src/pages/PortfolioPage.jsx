1a6deaf81c15ab7a83fcb9a65f0050e201d0fb64
Dusly Nestor <2395223@collegemv.qc.ca>
2025-05-06 13:49:57 -0400

    début format de ma page Portfolio

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -3,11 +3,26 @@ import React from 'react'
 function PortfolioPage() {
   return (
     <div>
-      <h1>portfolioPage</h1>
-      <h2></h2>
-      <h2></h2>
-      <h2></h2>
-      
+      <main>
+        <h1>portfolioPage</h1>
+        <section>
+          <h2>Présentation personnelle</h2>
+          {/**
+           *mon nom(peut être dans un h3)
+           une photo ou avatar (peut être dans un div)
+           quelques traits personnalité(sous le h3 de mon nom)
+           intérêts professionnels, passions, loisirs, etc(sous le h3 de mon nom)
+             */ }
+        </section>
+
+        <section>
+          <h2>Présentation professionnelle</h2>
+        </section>
+
+        <section>
+          <h2>Projets informatiques</h2>
+        </section>
+      </main>
     </div>
   )
 }
